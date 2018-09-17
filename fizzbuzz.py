9f2e6248fe0a0d259757c9aab1c0015109ac9ce6
peace <astonreba@gmail.com>
2018-09-17 14:24:33 +0300

    modified fizzbuzz.py to meet test requirements

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,9 @@
 def fizzbuzz(l1,l2):
-    len_1=str(len(l1))
-    len_2=str(len(l2))
+    len_1= len(l1)
+    len_2= len(l2)
     comb_len = int(len_1 +len_2)
-    
+    if isinstance(l1, list) or isinstance(l2, list):
+        return ("Invalid input")
     
     if (comb_len%3 == 0):
         return("Fizz")
@@ -11,6 +12,6 @@ def fizzbuzz(l1,l2):
     elif (comb_len%5==0 and comb_len%3==0): 
         return("Fizzbuzz")
     else:
-        return("comb_len")
+        return(comb_len)
 
-print(fizzbuzz([1,2],[1,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,33,33]))
+print(fizzbuzz([1,2],[1,2]))
