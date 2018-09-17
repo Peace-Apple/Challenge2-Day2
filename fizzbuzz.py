0906e9c5657452a0f657a4b628bd35ef41081c4f
peace <astonreba@gmail.com>
2018-09-17 14:41:22 +0300

    added an if statement to return invalid input if a list and a non-list are entered

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,25 @@
 def fizzbuzz(l1,l2):
+    if not (isinstance(l1, list) and isinstance(l2, list)):
+        return "Invalid input"
     len_1= len(l1)
     len_2= len(l2)
     comb_len = int(len_1 +len_2)
-    if isinstance(l1, list) or isinstance(l2, list):
-        return ("Invalid input")
-    
-    if (comb_len%3 == 0):
+   
+    if (comb_len %3 == 0):
         return("Fizz")
-    elif (comb_len%5 == 0):
+    elif (comb_len %5 == 0):
         return("Buzz")
-    elif (comb_len%5==0 and comb_len%3==0): 
+    elif (comb_len %5 ==0 and comb_len %3 ==0): 
         return("Fizzbuzz")
     else:
         return(comb_len)
 
-print(fizzbuzz([1,2],[1,2]))
+print(fizzbuzz([1,2],[1]))
+
+    
+
+   
+
+  
+
+
